74af4e133fc6416ac06ddb814b5cd66187b45300
jorgtho <jorgen.thorsnes@gmail.com>
2022-09-15 14:52:10 +0200

    added logging to getEmployee

--- GetEmployee/index.js
+++ GetEmployee/index.js
@@ -1,6 +1,8 @@
 const mongo = require('../lib/mongo')
 const { mongoDB, appRoles } = require('../config')
 const { verifyRoles } = require('../lib/verifyTokenClaims')
+const { logger, logConfig } = require('@vtfk/logger')
+
 
 const determineParam = (id) => {
   const emailRegex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])")
@@ -15,19 +17,28 @@ const determineParam = (id) => {
   }
 }
 
-let projection = {
-  userPrincipalName: 1,
-  fornavn: 1,
-  etternavn: 1,
-  officeLocation: 1,
-  'aktiveArbeidsforhold.hovedstilling': 1,
-  'aktiveArbeidsforhold.stillingstittel': 1,
-  'aktiveArbeidsforhold.arbeidssted.navn': 1,
-  'azureAd.manager.displayName': 1,
-  'azureAd.manager.userPrincipalName': 1,
-} 
-
 module.exports = async function (context, req) {
+  logConfig({
+    prefix: 'azf-user-info - GetEmployee',
+    azure: {
+      context,
+      excludeInvocationId: true
+    }
+  })
+  logger('info', [`new request - validating roles`])
+  
+  // Base projection
+  let projection = {
+    userPrincipalName: 1,
+    fornavn: 1,
+    etternavn: 1,
+    officeLocation: 1,
+    'aktiveArbeidsforhold.hovedstilling': 1,
+    'aktiveArbeidsforhold.stillingstittel': 1,
+    'aktiveArbeidsforhold.arbeidssted.navn': 1,
+    'azureAd.manager.displayName': 1,
+    'azureAd.manager.userPrincipalName': 1,
+  } 
   // Verify that the users have access to this endpoint
   if (verifyRoles(req.headers.authorization, [appRoles.admin, appRoles.priveleged])) {
     projection = {
@@ -43,21 +54,28 @@ module.exports = async function (context, req) {
       personalressurskategori: 1,
       harAktivtArbeidsforhold: 1
     }
+    logger('info', [`roles validated - will use expanded projection data`, projection])
+  } else {
+    logger('info', [`roles not present - will use base projection data`, projection])
   }
 
   if (!req.params.id) return { status: 400, body: 'Please specify query param {id} with an ssn, upn, or samAccountName' }
   const query = determineParam(req.params.id)
   if (!query) return { status: 400, body: 'Please specify VALID query param {id} with an ssn, upn, or samAccountName' }
-
+  
+  logger('info', [`running query for ${query.prop}: "${query.value}"`])
   const db = await mongo()
   const collection = db.collection(mongoDB.employeeCollection)
   try {
     const filteredDocs = await collection.find({ [query.prop]: query.value }).project(projection).toArray()
     if (filteredDocs.length === 0) {
-      return { status: 404, body: `No users found with "${query.prop}: "${query.value}"` }
+      logger('info', [`No users found with "${query.prop}: "${query.value}"`])
+      return { status: 404, body: `No users found with ${query.prop}: "${query.value}"` }
     }
+    logger('info', [`Found data for user ${query.prop}: "${query.value}"`])
     return { status: 200, body: filteredDocs[0] }
   } catch (error) {
+    logger('error', error.message)
     return { status: 500, body: error.message }
   }
 }
