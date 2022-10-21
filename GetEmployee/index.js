76628de5dd36ccf0b31195f6326bc84b35a90808
jorgtho <jorgen.thorsnes@gmail.com>
2022-10-21 14:52:26 +0200

    Leaders can now get more data on their employees

--- GetEmployee/index.js
+++ GetEmployee/index.js
@@ -1,22 +1,22 @@
 const mongo = require('../lib/mongo')
-const { mongoDB, appRoles, validUpnSuffix } = require('../config')
-const { verifyRoles, verifyUpn } = require('../lib/verifyTokenClaims')
+const { mongoDB, appRoles } = require('../config')
+const { verifyToken, isLeader } = require('../lib/verifyToken')
 const { logger, logConfig } = require('@vtfk/logger')
-const { baseProjection, expandedProjection, nameSearchProjection } = require('../lib/employee/employeeProjections')
+const { employeeProjection, nameSearchProjection, filterEmployeeData } = require('../lib/employee/employeeProjections')
 
 const determineParam = (id) => {
   const emailRegex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])")
   const samAccountRegex = new RegExp("[a-z]{2,3}[0-9]{4,5}")
   if (id.length === 11 && !isNaN(id)) { // SSN
-    return { query:  { 'fodselsnummer': id, harAktivtArbeidsforhold: true } }
+    return { query:  { 'fodselsnummer': id, harAktivtArbeidsforhold: true }, searchProjection: employeeProjection, type: 'unique' }
   } else if (id.length < 10 && !isNaN(id)) {
-    return { query: { 'ansattnummer': id, harAktivtArbeidsforhold: true } }
+    return { query: { 'ansattnummer': id, harAktivtArbeidsforhold: true }, searchProjection: employeeProjection, type: 'unique' }
   } else if (emailRegex.test(id)) {
-    return { query: { 'userPrincipalName': id, harAktivtArbeidsforhold: true } }
+    return { query: { 'userPrincipalName': id, harAktivtArbeidsforhold: true }, searchProjection: employeeProjection, type: 'unique' }
   } else if (samAccountRegex.test(id)) {
-    return { query: { 'samAccountName': id, harAktivtArbeidsforhold: true } }
+    return { query: { 'samAccountName': id, harAktivtArbeidsforhold: true }, searchProjection: employeeProjection, type: 'unique' }
   } else {
-    return { query: { navn: {'$regex' : id, '$options' : 'i'}, harAktivtArbeidsforhold: true }, searchProjection: nameSearchProjection }
+    return { query: { navn: {'$regex' : id, '$options' : 'i'}, harAktivtArbeidsforhold: true }, searchProjection: nameSearchProjection, type: 'search' }
   }
 }
 
@@ -29,38 +29,26 @@ module.exports = async function (context, req) {
     }
   })
   logger('info', ['new request - validating roles and upn'])
-  // Verify upn
-  const verUpn = verifyUpn(req.headers.authorization)
-  if (!verUpn) return { status: 401, body: 'You are not authorized to view this resource, upn suffix is not authorized' }
-
-  // Base projection
-  let projection = baseProjection
-  // Verify that the users have access to this endpoint
-  const priveleged = verifyRoles(req.headers.authorization, [appRoles.admin, appRoles.priveleged])
-  if (priveleged) {
-    // Expanded projection
-    projection = expandedProjection
-    logger('info', ['roles validated - will use expanded projection data'])
-  } else {
-    logger('info', ['roles not present - will use base projection data'])
-  }
+  // Verify token
+  const ver = verifyToken(req.headers.authorization)
+  if (!ver.verified) return { status: 401, body: `You are not authorized to view this resource, ${ver.msg}` }
+  
+  const privileged = ver.roles.includes(appRoles.admin) || ver.roles.includes(appRoles.privileged)
+  logger('info', ['checked if has privileged role - result', privileged])
 
   if (!req.params.id) return { status: 400, body: 'Please specify query param {id} with an ssn, upn, samAccountName, ansattnummer, or name' }
-  const { query, searchProjection } = determineParam(req.params.id)
+  const { query, searchProjection, type } = determineParam(req.params.id)
   if (!query) return { status: 400, body: 'Please specify VALID query param {id} with an ssn, upn, samAccountName, ansattnummer, or name' }
 
   // Check if can use ssn as query
-  if (!priveleged && query.fodselsnummer) return { status: 401, body: 'You are not authorized to use ssn as query' }
-
-  // Override projection if it the query is a partial search query
-  if (searchProjection) projection = searchProjection
+  if (!privileged && query.fodselsnummer) return { status: 401, body: 'You are not authorized to use ssn as query' }
 
   logger('info', [`running query for`, query])
   const db = mongo()
   let collection = db.collection(mongoDB.employeeCollection)
   let res = {}
   try {
-    const employeeData = await collection.find(query).limit(10).project(projection).toArray()
+    const employeeData = await collection.find(query).limit(10).project(searchProjection).toArray()
     if (employeeData.length === 0) {
       logger('info', ["No users found with", query])
       return { status: 404, body: `No users found with "${JSON.stringify(query)}"` }
@@ -71,12 +59,25 @@ module.exports = async function (context, req) {
     logger('error', error.message)
     return { status: 500, body: error.message }
   }
-  if (searchProjection) {
+
+  if (type === 'search') {
     logger('info', [`Using searchProjection`, query, 'do not need competence data'])
     return { status: 200, body: res }
-  } else if (!priveleged) {
-    logger('info', [`Not privileged`, query, 'do not need competence data'])
-    return { status: 200, body: res }
+  } else if (!privileged) {
+    // Check if has leader privilege
+    const structures = res[0].aktiveArbeidsforhold.map(forhold => {
+      return forhold.arbeidssted.struktur
+    })
+    const leaderPrivilege = isLeader(req.headers.authorization, structures, 2)
+
+    if (leaderPrivilege) {
+      logger('info', [`${ver.upn} is leader for ${res[0].userPrincipalName}`, 'Will expand result with competence data'])
+    }
+    else {
+      logger('info', [`Not privileged`, query, 'do not need competence data'])
+      res = filterEmployeeData(res)
+      return { status: 200, body: res }
+    }
   }
 
   // If privileged and specific query we expand with competence data
@@ -94,5 +95,5 @@ module.exports = async function (context, req) {
     logger('error', error.message)
     return { status: 500, body: error.message }
   }
-  return { status: 200, body: [res] }
+  return { status: 200, body: [ { ...res, isPrivileged: true } ] }
 }
