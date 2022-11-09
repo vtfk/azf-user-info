e4ead58559945454b6a45da995dc562d079b5ec4
jorgtho <jorgen.thorsnes@gmail.com>
2022-11-09 12:29:41 +0100

    Changeed cahce function to timer trigger

--- UpdateEmployeeCache/index.js
+++ UpdateEmployeeCache/index.js
@@ -1,23 +1,30 @@
 const { bakeEmployees } = require('../lib/employee/bakeEmployees')
+const bakeOrg = require('../lib/org/bakeOrg')
 const { mongoDB } = require('../config')
 const switchMainCollection = require('../lib/switchMainCollection')
 const { logger } = require('@vtfk/logger')
 const mongo = require('../lib/mongo')
 
-module.exports = async function (context, req) {
+module.exports = async function (context, myTimer) {
   try {
+    await logger('info', 'Starting updateEmployeeCache')
     const employeeData = await bakeEmployees()
 
     const res = await switchMainCollection(mongoDB.employeeCollection, employeeData)
+    await logger('info', ['Finished updateEmployeeCache', res])
+
+    await logger('info', 'Starting updateOrgCache')
+    const orgData = await bakeOrg()
+
+    const orgRes = await switchMainCollection(mongoDB.orgCollection, orgData)
+    await logger('info', ['Finished updateOrgCache', orgRes])
+    
     /*
     const db = mongo()
     const collection = db.collection(mongoDB.employeeCollection)
     const indexRes = await collection.createIndex( { fornavn: "text", etternavn: "text" } )
     */
-    return { status: 200, body: res }
   } catch (error) {
-    console.log(error)
     await logger('error', error.message)
-    return { status: 500, body: error.message }
   }
 }
