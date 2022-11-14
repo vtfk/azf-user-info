8f67ca6975585999477ff087f1e10a7f9d48811a
jorgtho <jorgen.thorsnes@gmail.com>
2022-11-14 21:53:32 +0100

    Added competence info to org

--- GetOrg/index.js
+++ GetOrg/index.js
@@ -1,5 +1,5 @@
 const mongo = require('../lib/mongo')
-const { mongoDB } = require('../config')
+const { mongoDB, appRoles } = require('../config')
 const { verifyToken } = require('../lib/verifyToken')
 const { logConfig, logger } = require('@vtfk/logger')
 
@@ -23,8 +23,10 @@ const orgProjection = {
   'arbeidsforhold.stillingstittel': 1,
   'arbeidsforhold.ansettelsesprosent': 1,
   'arbeidsforhold.personalressurskategori': 1,
-  'arbeidsforhold.officeLocation': 1
+  'arbeidsforhold.officeLocation': 1,
+  'arbeidsforhold.mandatoryCompetenceInput': 1,
 }
+
 const allProjection = {
   _id: 0,
   organisasjonsId: 1,
@@ -49,12 +51,17 @@ const determineParam = (param) => {
   if (!isNaN(param) || param === 'hoved') { // orgid
     return { query:  { 'organisasjonsId': param }, searchProjection: orgProjection, type: 'unique' }
   } else if (emailRegex.test(param)) {
-    return { query: { 'arbeidsforhold.userPrincipalName': param }, searchProjection: orgProjection, type: 'unique' }
+    return { query: { $or: [ { 'arbeidsforhold.userPrincipalName': param }, {'leder.userPrincipalName': param } ] }, searchProjection: orgProjection, type: 'unique' }
   } else if (param.toLowerCase() === 'all') {
     return { query: {}, searchProjection: allProjection, type: 'all' }
   } else {
     return { query: { navn: {'$regex' : param, '$options' : 'i'} }, searchProjection: orgProjection, type: 'search' }
   }
+}
+
+const isLeader = (upn, leaderUpn) => {
+  if (upn === leaderUpn) return true
+  else return false
 }
 
 module.exports = async function (context, req) {
@@ -70,6 +77,9 @@ module.exports = async function (context, req) {
   const ver = verifyToken(req.headers.authorization)
   if (!ver.verified) return { status: 401, body: `You are not authorized to view this resource, ${ver.msg}` }
 
+  let privileged = ver.roles.includes(appRoles.admin) || ver.roles.includes(appRoles.privileged)
+  logger('info', ['checked if has privileged role - result', privileged])
+
   const { query, searchProjection, type } = determineParam(req.params.param)
   logger('info', [query, type])
 
@@ -79,16 +89,17 @@ module.exports = async function (context, req) {
   try {
     const org = await collection.find(query).project(searchProjection).toArray()
     if (type === 'all') return { status: 200, body: org }
-
+    
+    // If several returned - quick return result
+    if (org.length > 1) { return { status: 200, body: org } }
     const positionIds = org.map(unit => {
       return unit.arbeidsforhold.map(forhold => forhold.systemId)
     })
 
-
     const taskQuery = { "positionTasks.positionId": { "$in": positionIds.flat() } }
     collection = db.collection(mongoDB.competenceCollection)
     const posTasks = await collection.find(taskQuery).project(taskProjection).toArray()
-    const orgRes = org.map(unit => {
+    let orgRes = org.map(unit => {
       return {
         ...unit,
         arbeidsforhold: unit.arbeidsforhold.map(forhold => {
@@ -99,7 +110,30 @@ module.exports = async function (context, req) {
         })
       }
     })
+    privileged = privileged || isLeader(ver.upn, orgRes[0].leder.userPrincipalName)
 
+    if (!privileged) return { status: 200, body: orgRes }
+
+    const competenceProjection = {
+      _id: 0,
+      "other.soloRole": 1,
+      perfCounty: 1,
+      userPrincipalName: 1
+    }
+    const competence =  await collection.find({}).project(competenceProjection).toArray()
+    orgRes = orgRes.map(unit => {
+      return {
+        ...unit,
+        arbeidsforhold: unit.arbeidsforhold.map(forhold => {
+          const comp = competence.find(c => c.userPrincipalName === forhold.userPrincipalName)
+          return {
+            ...forhold,
+            soloRole: comp?.other?.soloRole ?? null,
+            perfCounty: comp?.perfCounty ?? null
+          }
+        })
+      }
+    })
     return { status: 200, body: orgRes }
   } catch (error) {
     return { status: 500, body: error.message }
