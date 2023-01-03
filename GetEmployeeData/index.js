74643eebb295521c49059b179361e19cc9cb393b
jorgtho <jorgen.thorsnes@gmail.com>
2023-01-03 10:44:10 +0100

    Added example data

--- GetEmployeeData/index.js
+++ GetEmployeeData/index.js
@@ -3,6 +3,21 @@ const { mongoDB, appRoles, leaderLevel } = require('../config')
 const { verifyAppToken } = require('../lib/verifyToken')
 const { logger, logConfig } = require('@vtfk/logger')
 
+const repackWorkExperience = workExperience => {
+  if (!workExperience) return 'Ingen tidligere arbeidsforhold'
+  let expString = ''
+  for (const exp of workExperience) {
+    expString += `• ${exp.fromYear} - ${exp.toYear}. ${exp.position} - ${exp.employer}\n`
+  }
+  return expString
+}
+
+const repackResult = (employee, competence) => {
+  return {
+    employee: employee.navn,
+    workExperience: repackWorkExperience(competence?.workExperience)
+  }
+}
 
 module.exports = async function (context, req) {
   logConfig({
@@ -18,34 +33,56 @@ module.exports = async function (context, req) {
   if (!ver.verified) return { status: 401, body: `You are not authorized to view this resource, ${ver.msg}` }
   
   const hasAccess = ver.roles.includes(appRoles.applicationRead)
-  await logger('info', [ver.appid, 'checked if has privileged role - result', hasAccess])
+  logger('info', [ver.appid, 'checked if has privileged role - result', hasAccess])
   if (!hasAccess) return { status: 401, body: `You are not authorized to view this resource, required role missing` }
 
   if (!req.body) return { status: 400, body: 'body is required' }
   const upnString = req.body
 
-  await logger('info', [ver.appid, 'this is the body', req.body])
-
   if (!upnString) return { status: 400, body: 'Missing required parameter "upnString"' }
 
   const upns = upnString.split(';')
-  if (upns.length === 2) {
-    return { status: 200, body: { managerUpn: upns[0], employeeUpn: upns[1] } }
-  } else {
-    return { status: 400, body: 'upnString needs to be on the format "manager@company.com;employee@company.com' }
-  }
+  if (upns.length !== 2) return { status: 400, body: 'upnString needs to be on the format "manager@company.com;employee@company.com' }
+  // Da sjekker vi om manager er sjef for employee - hvis den er det - så returnerer vi kompetanse-data, hvis ikke, så sier vi at den må legge det inn selv, eller være sjef
 
-  /*
-  const { upn, ssn, fullName } = req.body
-
-  if (!upn || !fullName) return { status: 400, body: 'Missing required parameter "upn" or "fullName"' }
-  if (!ssn) return { status: 400, body: 'Missing required parameter "ssn"' }
+  const managerUpn = upns[0]
+  const employeeUpn = upns[1]
 
   const db = mongo()
   let collection = db.collection(mongoDB.employeeCollection)
-  let query = { "$or": { "navn": (fullName || 'ingen som heter detta her da'), "userPrincipalName": (upn || 'detta her er ikke et upn') }, "fodselsnummer": ssn }
-  const employee = await collection.find({  }).toArray()
+  let query = { userPrincipalName: employeeUpn, harAktivtArbeidsforhold: true }
+  const employee = await collection.findOne(query)
+  if (!employee) {
+    logger('info', [ver.appid, `Could not find employee for ${employeeUpn}`])
+    return { status: 404, body: `Could not find employee for ${employeeUpn}` }
+  }
+  logger('info', [ver.appid, `Found employeeData for ${employeeUpn}, checking if ${managerUpn} is manager`])
 
-  return { status: 200, body: { littData: "hallo", littmer: "funker det???", nesta: { tut: "burt" } } }
-  */
+  // Vi går gjennom strukrurer for aktive arbeidsforhold - sjekker om managerUpn ligger i maks nivå "variabel" som leder for den ansatte
+  let isLeader = false
+  for (const forhold of employee.aktiveArbeidsforhold) {
+    const leaders = forhold.arbeidssted.struktur.slice(0, leaderLevel).map(unit => unit.leder)
+    if (leaders.includes(managerUpn)) isLeader = true
+  }
+  if (!isLeader) {
+    logger('info', [ver.appid, `${managerUpn} is NOT manager for ${employeeUpn}, will not return employeeData`])
+    return { status: 200, body: `Du er ikke leder for ${employee.navn}, og får derfor ikke hente data fra kompetansemodulen` }
+  }
+
+  // ManagerUpn er leder for employeeUpn innen nivå leaderLevel, da henter vi kompetansedata og sender tilbake
+  collection = db.collection(mongoDB.competenceCollection)
+  query = { fodselsnummer: employee.fodselsnummer }
+  const competence = await collection.findOne(query)
+
+  if (!competence) {
+    logger('info', [ver.appid, `Could not find competencedata for ${employeeUpn}`])
+    return { status: 200, body: `Fant ansatt ${employee.navn}, men ansatt har ikke fylt ut kompetanse i kompetansemodulen` }
+  }
+
+  logger('info', [ver.appid, `${managerUpn} is manager for ${employeeUpn}, will return employeeData`])
+
+  const result = repackResult(employee, competence)
+
+  return { status: 200, body: result }
+
 }
