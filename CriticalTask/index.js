0defa8fe7aadcfc3c3bc4829f0ae46b81c72a1ba
jorgtho <jorgen.thorsnes@gmail.com>
2023-01-12 17:49:05 +0100

    modified endpoint for saving criticaltasks

--- CriticalTask/index.js
+++ CriticalTask/index.js
@@ -1,5 +1,5 @@
 const mongo = require('../lib/mongo')
-const { mongoDB, appRoles } = require('../config')
+const { mongoDB } = require('../config')
 const { verifyToken } = require('../lib/verifyToken')
 const { logger, logConfig } = require('@vtfk/logger')
 
@@ -13,36 +13,55 @@ module.exports = async function (context, req) {
     }
   })
 
-  const unit = context.bindingData.param.toString()
-  logger('info', ['checking that unit id is provided', unit])
   // Verify token
   const ver = verifyToken(req.headers.authorization)
   if (!ver.verified) return { status: 401, body: `You are not authorized to view this resource, ${ver.msg}` }
   
-  const privileged = ver.roles.includes(appRoles.admin)
-  logger('info', ['checked if has privileged role - result', privileged])
-  if (!privileged) return { status: 401, body: `You do not have sufficient permissions to use this resource` }
+  // Verify payload
+  if (typeof req.body !== 'object') return { status: 400, body: 'That is not an object body' }
+  if (!Array.isArray(req.body)) return { status: 400, body: 'Object body must be of type array' }
+  for (const task of req.body) {
+    if (!task.hasOwnProperty('criticalTask') || !task.hasOwnProperty('ansattnummer')) return { status: 400, body: 'Array elements must be on the form: { ansattnummer: "1234567", criticalTask: true/false }' }
+  }
+  
+  // Create list of employeeNumbers to update
+  const employeeNumbers = req.body.map(criticalTask => criticalTask.ansattnummer)
 
-  const db = mongo()
-  const collection = db.collection(mongoDB.criticalTasksCollection)
+  logger('info', ["Trying to update critical tasks"])
+  try {
+    // Get existing critical tasks
+    const db = mongo()
+    const collection = db.collection(mongoDB.criticalTasksCollection)
+    const criticalQuery = { ansattnummer: { $in: employeeNumbers } }
+    const criticalTasks = await collection.find(criticalQuery).project({ _id: 0 }).toArray()
+    
+    // Filter out unedited critical tasks
+    const editedCriticalTasks = []
+    for (const task of req.body) {
+      const correspondingTask = criticalTasks.find(t => t.ansattnummer === task.ansattnummer)
+      if (correspondingTask) {
+        if (correspondingTask.criticalTask !== task.criticalTask) editedCriticalTasks.push(task)
+      } else {
+        editedCriticalTasks.push(task)
+      }
+    }
 
-  if (req.method === 'GET') {
-    try {
-      logger('info', ["Trying to GET critical tasks"])
-      const getRes = await collection.find({ unitId: unit }).toArray()
-      return { status: 200, body: getRes }
-    } catch (error) {
-      return { status: 500, body: error.message }
-    }
-  } else if (req.method === 'POST') {
-    try {
-      logger('info', ["Trying to update critical tasks"])
-      const upsertRes = await collection.findOneAndReplace({ unitId: unit }, { ...req.body}, { upsert: true })
-      return { status: 200, body: upsertRes }
-    } catch (error) {
-      return { status: 500, body: error.message }
-    }
-  } else {
-    return { status: 400, body: "Not a legal method" }
+    // Create bulkwrite operation for mongo to handle
+    const bulkOperation = editedCriticalTasks.map(task => {
+      return {
+        updateOne: {
+          filter: { ansattnummer: task.ansattnummer },
+          update: { $set: { ansattnummer: task.ansattnummer, criticalTask: task.criticalTask, modifiedBy: ver.upn, timestamp: new Date().toISOString() } },
+          upsert: true
+        }
+      }
+    })
+
+    if (bulkOperation.length === 0) return { status: 200, body: 'Ingen endringer' }
+
+    const upsertRes = await collection.bulkWrite(bulkOperation)
+    return { status: 200, body: upsertRes }
+  } catch (error) {
+    return { status: 500, body: error.message }
   }
 }
